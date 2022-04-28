5b4cdb113f8dea91a761f063cc4908d79c7e2fec
Felipe Santos <rfelipe273@gmail.com>
2022-04-27 22:39:30 -0300

    add screen with button to sign in or sign out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectUserEmail,
+  selectUserImage,
+  selectUserName,
+  setActiveUser,
+  setUserLogoutState,
+} from "./features/userSlice";
+
+import { auth, provider } from "./firebase";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const userName = useSelector(selectUserName);
+
+  const handleSignIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        dispatch(
+          setActiveUser({
+            userName: user.displayName,
+            userEmail: user.email,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(setUserLogoutState());
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {userName ? (
+          <>
+            <h1>Hello, {userName}. Have a good day.</h1>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </>
+        ) : (
+          <>
+            <h1>Sign In, please.</h1>
+            <button onClick={handleSignIn}>Sign In</button>
+          </>
+        )}
       </header>
     </div>
   );
